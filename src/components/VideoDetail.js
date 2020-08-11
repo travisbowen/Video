44767d716424b96fd65bad8bf7ddac60e8cbf22d
Travis Bowen <Travisbowen23@gmail.com>
2020-08-11 13:19:41 -0700

    Added embedded youtube iframe video to video detail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,6 +7,11 @@ const VideoDetail = (props) => {
 
 	return (
 		<div>
+			<div className='ui embed'>
+				<iframe
+					src={`https://www.youtube.com/embed/${props.video.id.videoId}`}
+				/>
+			</div>
 			<div className='ui segment'>
 				<h4 className='ui header'>{props.video.snippet.title}</h4>
 				<p>{props.video.snippet.description}</p>
