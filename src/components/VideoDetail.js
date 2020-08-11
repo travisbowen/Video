2cb02c8d3d9a96192d4b3f8f8293b33c6b2f69ca
Travis Bowen <Travisbowen23@gmail.com>
2020-08-11 13:13:41 -0700

    Added style to video detail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,7 +5,14 @@ const VideoDetail = (props) => {
 		return <div>Loading...</div>;
 	}
 
-	return <div>{props.video.snippet.title}</div>;
+	return (
+		<div>
+			<div className='ui segment'>
+				<h4 className='ui header'>{props.video.snippet.title}</h4>
+				<p>{props.video.snippet.description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default VideoDetail;
