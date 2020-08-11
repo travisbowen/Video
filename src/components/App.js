ba4cffb1fb76846dc6e0db5ad72b9ed26fbeb745
Travis Bowen <Travisbowen23@gmail.com>
2020-08-11 13:50:49 -0700

    Styled child components within the app component

--- src/components/App.js
+++ src/components/App.js
@@ -35,11 +35,19 @@ class App extends React.Component {
 		return (
 			<div className='ui container'>
 				<SearchBar onFormSubmit={this.onSearchTermSubmit} />
-				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList
-					onVideoSelected={this.onVideoSelected}
-					videos={this.state.videos}
-				/>
+				<div className='ui grid'>
+					<div className='ui row'>
+						<div className='eleven wide column'>
+							<VideoDetail video={this.state.selectedVideo} />
+						</div>
+						<div className='five wide column'>
+							<VideoList
+								onVideoSelected={this.onVideoSelected}
+								videos={this.state.videos}
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
